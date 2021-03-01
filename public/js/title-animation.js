b70b6912907f61ff839ca638574cc80fc4b9ee17
Mohammed Ali Jaafar <mohammedalijaafar1999@gmail.com>
2021-03-01 11:45:19 +0300

    logo sizing and space issues to fix with background video glitching

--- public/js/title-animation.js
+++ public/js/title-animation.js
@@ -1,59 +1,59 @@
 // Wrap every letter in a span for FN Studio
-var textWrapper = document.querySelector('.ml3');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+// var textWrapper = document.querySelector('.ml3');
+// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({
-        loop: false,
-    })
-    .add({
-        targets: '.ml3 .letter',
-        opacity: [0,0],
-        duration:2000,
-        easing: "linear"
-    })
-    .add({
-        targets: '.ml3 .letter',
-        opacity: [0,1],
-        easing: "easeInOutQuad",
-        duration: 2250,
-        delay: (el, i) => 150 * (i+1)
-    });
+// anime.timeline({
+//         loop: false,
+//     })
+//     .add({
+//         targets: '.ml3 .letter',
+//         opacity: [0,0],
+//         duration:2000,
+//         easing: "linear"
+//     })
+//     .add({
+//         targets: '.ml3 .letter',
+//         opacity: [0,1],
+//         easing: "easeInOutQuad",
+//         duration: 2250,
+//         delay: (el, i) => 150 * (i+1)
+//     });
 
 // Wrap every letter in a span for intro p
 var textWrapperp = document.querySelector('.ml3t');
 textWrapperp.innerHTML = textWrapperp.textContent.replace(/\S/g, "<span class='letterp'>$&</span>");
 
 anime.timeline({
-        loop: false,
-    })
+    loop: false,
+})
     .add({
         targets: '.ml3t .letterp',
-        opacity: [0,0],
-        duration:3500,
+        opacity: [0, 0],
+        duration: 3500,
         easing: "linear"
     })
     .add({
         targets: '.ml3t .letterp',
-        opacity: [0,1],
+        opacity: [0, 1],
         easing: "easeInOutQuad",
         duration: 2250,
-        delay: (el, i) => 100 * (i+1)
+        delay: (el, i) => 100 * (i + 1)
     });
 
 // Wrap every letter in a span for intro p
 anime.timeline({
-        loop: false,
-    })
+    loop: false,
+})
     .add({
         targets: 'menu-item',
-        opacity: [0,0],
-        duration:3500,
+        opacity: [0, 0],
+        duration: 3500,
         easing: "linear"
     })
     .add({
         targets: '.ml3t .letterp',
-        opacity: [0,1],
+        opacity: [0, 1],
         easing: "easeInOutQuad",
         duration: 2250,
-        delay: (el, i) => 100 * (i+1)
+        delay: (el, i) => 100 * (i + 1)
     });
